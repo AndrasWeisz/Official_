a0b3f80ee80609b48faf5c7d12285c49e5a37c3d
corvinus-p97bbn <79064254+corvinus-p97bbn@users.noreply.github.com>
2022-12-13 12:27:32 +0100

    Hozzáadás funkció

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +21,25 @@ namespace UserMaintenance
             label1.Text = Resource1.LastName;
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
+            listBox1.DataSource = users;
+            listBox1.DisplayMember = "FullName";
+            listBox1.ValueMember = "ID";
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FristName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
