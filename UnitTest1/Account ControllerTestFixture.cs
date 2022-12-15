fe548f39b3cbec83384567e18eed798bef493846
corvinus-p97bbn <79064254+corvinus-p97bbn@users.noreply.github.com>
2022-12-15 10:06:24 +0100

    Jelszó validálás függvény

--- UnitTest1/Account ControllerTestFixture.cs
+++ UnitTest1/Account ControllerTestFixture.cs
@@ -10,12 +10,30 @@ namespace UnitTest1
 {
     public class Account_ControllerTestFixture
     {
-        [Test]
-        private void TestValidateEmail(string email, bool expectedResult)
+        [Test,
+            TestCase("abcd1234",false),
+            TestCase("ifr@uni-corvinus",false),
+            TestCase("irf.uni-corvunis.hu",false),
+            TestCase("ifr@uni-corvinus.hu",true)
+            ]
+        public void TestValidateEmail(string email, bool expectedResult)
         {
             var accountController = new AccountController();
             var actualResult = accountController.ValidateEmail(email);
             Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test,
+            TestCase("abcdABCD", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcd1234", false),
+            TestCase("Abc1234", false),
+            TestCase("Abcd1234", true)
+            ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+            var actualResult2 = accountController.ValidatePassword(password);
+            Assert.AreEqual(expectedResult, actualResult2);
         }
     }
 }
